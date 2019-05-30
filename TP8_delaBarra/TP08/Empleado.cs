b5419aaac4fd07740dcbe4180f1d346c202579fc
Augusto Orso <augustorso94@gmail.com>
2019-05-30 19:37:48 -0300

    Trabajo Practico 8

---
+++ TP8_delaBarra/TP08/Empleado.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP08
+{
+
+    class Empleado
+    {
+        public string Nombre, Apellido;
+        public Genero genero;
+        public Cargo cargo;
+        public int estadoCivil, sueldo, cantHijos;
+        public DateTime fechaIngreso, fechaNac;
+
+        public Empleado(string _Nombre, string _Apellido, DateTime _fechaNac, int _estadoCivil, int _cantHijos, Genero _genero, int _sueldo, Cargo _cargo, DateTime _fechaIngreso)
+        {
+            Nombre = _Nombre;
+            Apellido = _Apellido;
+            fechaNac = _fechaNac;
+            estadoCivil = _estadoCivil;
+            cantHijos = _cantHijos;
+            genero = _genero;
+            sueldo = _sueldo;
+            cargo = _cargo;
+            fechaIngreso = _fechaIngreso;
+
+        }
+
+        //Calculo de edad
+
+        public static int edadEmpleado(Empleado empleado)
+        {
+            DateTime fechaActual = DateTime.Now;
+            int edad = 0;
+            if (empleado.fechaNac.Month < fechaActual.Month)
+            {
+                edad = fechaActual.Year - empleado.fechaNac.Year;
+            }
+            else if (empleado.fechaNac.Month == fechaActual.Month)
+            {
+                if (empleado.fechaNac.Day <= fechaActual.Day)
+                {
+                    edad = fechaActual.Year - empleado.fechaNac.Year;
+                }
+                else
+                {
+                    edad = fechaActual.Year - empleado.fechaNac.Year - 1;
+                }
+            }
+            else
+            {
+                edad = fechaActual.Year - empleado.fechaNac.Year - 1;
+            }
+            return edad;
+        }
+
+        //Calculo de Antiguedad
+        public static double antiguedad(Empleado empleado)
+        {
+            DateTime fechaActual = DateTime.Now;
+            double antiguedadEmp;
+
+            antiguedadEmp = (fechaActual.Year - empleado.fechaIngreso.Year);
+
+            return antiguedadEmp;
+        }
+
+        //Calculo de jubilacion
+
+        public static int calculoJubilacion(Empleado empleado)
+        {
+            DateTime fechaActual = DateTime.Now;
+            int edadAct = edadEmpleado(empleado);
+            if (empleado.genero == Genero.Femenino)
+            {
+                if (edadAct >= 60) return 0; else return 60 - edadAct;
+            }
+            else
+            {
+                if (edadAct >= 65) return 0; else return 65 - edadAct;
+            }
+        }
+
+        //Calculo del Salario
+        public static double salario(Empleado empleado)
+        {
+            double Salario, Adicional, antiguedad;
+            antiguedad = (Empleado.antiguedad(empleado)) / 10;
+
+            if (antiguedad < 20)
+            {
+                Adicional = empleado.sueldo * (0.2 * antiguedad);
+            }
+            else
+            {
+                Adicional = (empleado.sueldo * 0.25) * antiguedad;
+            }
+            if (empleado.cargo == Cargo.Ingeniero || empleado.cargo == Cargo.Especialista)
+            {
+                Adicional = Adicional + (Adicional * 0.50);
+            }
+            if (empleado.estadoCivil == 1 && empleado.cantHijos > 2) Adicional = Adicional + 5000;
+
+            Salario = empleado.sueldo + Adicional;
+
+            return Salario;
+        }
+
+        public static void mostrarEmpleado(Empleado empleado)
+        {
+            Console.WriteLine("Nombre y apellido: {0} {1}", empleado.Nombre, empleado.Apellido);
+            Console.WriteLine("Fecha de nacimiento: {0}", empleado.fechaNac.ToShortDateString());
+            Console.WriteLine("Estado Civil: {0}", empleado.estadoCivil);
+            Console.WriteLine("Genero: {0}", empleado.genero);
+            Console.WriteLine("Sueldo: {0}", empleado.sueldo);
+            Console.WriteLine("Cargo: {0}", empleado.cargo);
+            Console.WriteLine("Fecha de ingreso: {0}", empleado.fechaIngreso.ToShortDateString());
+        }
+        public static void busquedaEmpleado(List<Empleado> empleadoLista, string[] delimitador)
+        {
+            int indice = empleadoLista.FindIndex(x => x.Nombre.Contains(delimitador[0]) && x.Apellido.Contains(delimitador[1]));
+            try
+            {
+                Empleado.mostrarEmpleado(empleadoLista[indice]);
+                Console.WriteLine("\nEmpleado n. {0}\n", indice + 1);
+                Console.WriteLine("La edad del empleado es de: {0}", edadEmpleado(empleadoLista[indice]));
+                Console.WriteLine("El empleado tiene {0} años de antiguedad.", Empleado.antiguedad(empleadoLista[indice]));
+                Console.WriteLine("Al empleado le faltan {0} años para jubilarse.\n", calculoJubilacion(empleadoLista[indice]));
+
+            }catch(ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("El empleado {0}, {1} no existe en la base de datos.", delimitador[0], delimitador[1]);
+            }
+            
+        }
+    }
+}
---
+++ TP8_delaBarra/TP08/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP08
+{
+    public enum Cargo { Auxiliar = 1, Administrativo, Ingeniero, Especialista, Investigador };
+    public enum Genero { Masculino = 1, Femenino };
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<Empleado> empleadoLista = new List<Empleado>();
+            Empleado empleado;
+            int opc = 0;
+            string busqueda, rutaCSV;
+            string[] archivoCSV, cadenaEmpl;
+            string[] delimitador;
+
+            rutaCSV = "NuevosEmpleados.csv";
+            archivoCSV = File.ReadAllLines(rutaCSV);
+
+            cadenaEmpl = archivoCSV[0].Split(';');//tamaño 0-8
+
+            empleado = empleadoArchivo(cadenaEmpl);
+
+
+            //|========== Creando el empleado ==========|
+
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    empleado = crearEmpleado();
+            //    Console.WriteLine("\n|====== Empleado n. {0} =======|", i+1);
+            //    Empleado.mostrarEmpleado(empleado);
+            //    empleadoLista.Add(empleado);
+            //    Console.ReadKey();
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine("\n|===== Buscador de empleados =====|");
+            //    Console.WriteLine("\nIngrese el nombre del empleado que desea buscar: ");
+            //    busqueda = Console.ReadLine();
+            //    delimitador = busqueda.Split(' ');
+
+            //    Empleado.busquedaEmpleado(empleadoLista, delimitador);
+            //    Console.WriteLine("\n¿Desea continuar con la busqueda?: (1- Si ; 0- No)");
+            //    opc = int.Parse(Console.ReadLine());
+            //} while (opc != 0);
+
+
+            Console.ReadKey();
+
+        }
+
+        private static Empleado crearEmpleado()
+        {
+            string Nombre, Apellido;
+            string[] nombres = { "Pablo", "Esteban", "Tyrion", "Will", "Jon"};
+            string[] apellidos = { "Quito", "Tormout", "Taly", "White", "Snow"};
+            Genero genero;
+            Cargo cargo;
+            int estadoCivil, sueldo, cantHijos;
+            DateTime fechaIngreso, fechaNac;
+            Empleado cargaEmpleado;
+            Random rnd = new Random();
+
+            Nombre = nombres[rnd.Next(5)]; //Nombre
+
+            Apellido = apellidos[rnd.Next(5)]; // Apellido
+            
+            fechaNac = new DateTime(rnd.Next(1950, 1990), rnd.Next(1, 12), rnd.Next(1, 28)); //Fecha de nacimiento
+
+            estadoCivil = rnd.Next(1, 2); //Estado Civil
+
+            genero =(Genero) rnd.Next(1, 2); //Genero
+            
+            sueldo = rnd.Next(8000, 50000); //Sueldo
+            
+            cantHijos = rnd.Next(0, 10); //cant de hijos
+            cargo = (Cargo)rnd.Next(1, 5); //Cargando el cargo del empleado
+            
+            fechaIngreso = new DateTime(rnd.Next(1980, 2018), rnd.Next(1, 12), rnd.Next(1, 28)); //Inicializo la fecha de ingreso
+
+            cargaEmpleado = new Empleado(Nombre, Apellido, fechaNac, estadoCivil, cantHijos, genero, sueldo, cargo, fechaIngreso);
+
+            return cargaEmpleado;
+        }
+
+        public static Empleado empleadoArchivo(string[] cadenaEmpleado)
+        {
+            Empleado empleadoAux;
+
+        }
+    }
+}
